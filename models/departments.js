2eb0ec3998acd0872f364fa294a3cf7e13c102db
omar73 <omar.alaskndrani.1@gmail.com>
2020-12-18 17:35:28 +0200

    adding faculy and department schemas

---
+++ models/departments.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// department schema
+const departmentSchema=new mongoose.Schema({
+    departmentName:{
+        type:String,
+        required:true,
+      },
+    headID:{  // one to one ? then schema but its better to avoid anomalies
+        type:Number,
+        required:true,
+      },
+    course:[courseSchema]
+   });
+
+module.exports = mongoose.model('department',departmentSchema);
---
+++ models/faculties.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// FACULTY
+const facultySchema=new mongoose.Schema({
+    facultyName:
+            {type:String,
+            required:true,
+            },
+    deanID:
+            {type:String,
+            required:true,
+            },
+    departments: [departmentSchema]
+  });
+
+  module.exports = mongoose.model('Faculty',facultySchema);
+  
