d890357a4ae42d67abcf7227ab15cf899384c1bc
Omarsawan <o.sawan7@gmail.com>
2020-12-19 14:03:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ const connectionParams={
     useUnifiedTopology:true
 }
 
-const url="mongodb+srv://abdoolelcool:<password>@cluster0.vf8uu.mongodb.net/<dbname>?retryWrites=true&w=majority"
+// const url="mongodb+srv://abdoolelcool:<password>@cluster0.vf8uu.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const url="mongodb+srv://sawan:7777777o@cluster0.d4j6s.mongodb.net/TheSawanDB?retryWrites=true&w=majority"
 
 
 mongoose.connect(url,connectionParams).then(()=>{
@@ -32,6 +33,29 @@ mongoose.connect(url,connectionParams).then(()=>{
 }).catch(()=>{
     console.log("error in database connection");
 }); 
+
+app.post('/schedule',(req,res)=>{
+    // const location=new Location({
+    //     locationName:"C7",
+    //         size:40,
+    //         locationType:"exams"
+    // })
+    const slot=new Slot({
+        day:req.body.day,
+        time:req.body.time,
+        // location:location,
+        course:req.body.course
+    })
+    slot.save().then((data)=>{
+        res.json(data);
+    }).catch((error)=>{
+        res.json(error);
+    });
+})
+
+app.get('/schedule',(req,res)=>{
+    
+})
 
 module.exports.app = app
 //module.export = app;
