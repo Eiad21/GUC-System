e1eabf634325be327dc9849ca1b40b036b3cfb7b
omar73 <omar.alaskndrani.1@gmail.com>
2020-12-23 15:29:55 +0200

    saba7 eld7k 3lek

--- routes/InstructorRoutes.js
+++ routes/InstructorRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router();
 // const Location = require("../models/locationSchema").constructor
 const FacultyModel = require("../models/facultySchema").constructor;
+const MemberModel = require("../models/memberSchema").constructor;
 // const Department = require("../models/departmentSchema").constructor
 
 // Instructor Routes
@@ -55,8 +56,43 @@ router.get('/viewCoverage', async (req,res)=>{
     res.send(coverages);
 })
 
+// input member
+router.get('/viewCoursesAssignments', async (req,res)=>{
+    // getting the member requesting this get from the data base by the token
+    // and putting it in a variable  req.member using middleware
+    if(req.member.role != "instructor")
+    {
+        return res.status(401).send("Access denied!");
+    }
+    const facultyName = req.member.facultyName;
+    const departmentName = req.member.departmentName;
+    const depCourses = getCoursesInDep(facultyName,departmentName);
+    const schedules = {};
+    depCourses.forEach((course)=>{
+        if(isInstructorOfCourse(course,req.member.memberID))
+        {
+            const {courseName,courseSchedule} = course;
+            schedules[courseName] = courseSchedule;
+        }
+    });
+    res.send(schedules);
+})
 
-// waiting for kimOo to take code for the second functionality
+// input member & courseName
+router.get('/viewOneCourseAssignments', async (req,res)=>{
+    // getting the member requesting this get from the data base by the token
+    // and putting it in a variable  req.member using middleware
+    if(req.member.role != "instructor")
+    {
+        return res.status(401).send("Access denied!");
+    }
+    const facultyName = req.member.facultyName;
+    const departmentName = req.member.departmentName;
+    const depCourses = getCoursesInDep(facultyName,departmentName);
+    const course = depCourses.find(cou => cou.courseName == req.body.courseName);
+    res.send(course.courseSchedule);
+})
+
 //input member
 router.get('/viewDepartmentStaff', async (req,res)=>{
     // getting the member requesting this get from the data base by the token
@@ -145,9 +181,17 @@ router.post('/assignAcademicMember', async (req,res)=>{
    // fac.departments;
     const department = fac.departments.find(dep => dep.departmentName == departmentName);
     const course = department.courses.find(course => course.courseName == req.body.courseName);
+    if(!isInstructorOfCourse(course,req.member.memberID))
+    {
+        return res.status(401).send("You are not an instructor for this course!");
+    }
     course.courseSchedule.forEach((slot,idx)=>{
         if(slot.slotID == req.body.slotID)
         {
+            if(slot.assignedMemberID)
+            {
+               return res.status(401).send("Cannot change already assigned slot!");
+            }
             course.courseSchedule[idx]={
                 slotID: slot.slotID,
                 day: slot.day,
@@ -156,6 +200,16 @@ router.post('/assignAcademicMember', async (req,res)=>{
                 assignedMemberID: req.body.assignedMemberID,
                 assignedMemberName: req.body.assignedMemberID
             };
+            // update the schedule of this member ---> adding this slot to it
+            const member = await MemberModel.findOne({memberID: req.body.assignedMemberID});
+            const tailoredSlot = {
+                day:slot.day,
+                time:slot.time,
+                location:slot.location,
+                courseName:course.courseName
+            };
+            member.schedule.push(tailoredSlot);
+            await member.save();
         }
     });
     department.courses.forEach((courseItem,idx)=>{
@@ -170,26 +224,182 @@ router.post('/assignAcademicMember', async (req,res)=>{
             fac.departments[idx] = department
         }
     });
-    
-    Location.findOneAndUpdate(
-        {locationName: req.body.locationNameOld},
-        {
-            locationName:req.body.locationNameNew,
-            size:req.body.size,
-            locationType:req.body.locationType
-        },
-        { new: true },)
-        
-        .then((doc) => {
-            console.log(doc);
-            res.send(doc)
-          })
-          .catch((err) => {
-              console.error(err);
-              res.send(err)
-        }
-    );
+    await fac.save();
 });
+
+
+router.delete('/assignAcademicMember', async (req,res)=>{
+    // getting the member requesting this get from the data base by the token
+    // and putting it in a variable  req.member using middleware
+    if(req.member.role != "instructor")
+    {
+        return res.status(401).send("Access denied!");
+    }
+    const facultyName = req.member.facultyName;
+    const departmentName = req.member.departmentName;
+    const fac =await FacultyModel.findOne({facultyName: facultyName});
+   // fac.departments;
+    const department = fac.departments.find(dep => dep.departmentName == departmentName);
+    const course = department.courses.find(course => course.courseName == req.body.courseName);
+    if(!isInstructorOfCourse(course,req.member.memberID))
+    {
+        return res.status(401).send("You are not an instructor for this course!");
+    }
+    course.courseSchedule.forEach((slot,idx)=>{
+        if(slot.slotID == req.body.slotID)
+        {
+            // if(!slot.assignedMemberID)
+            // {
+            //     return res.status(401).send("This slot is already unassigned!");
+            // }
+            const oldAssignedMemberID = slot.assignedMemberID;
+            course.courseSchedule[idx]={
+                slotID: slot.slotID,
+                day: slot.day,
+                time: slot.time,
+                location: slot.location,
+                assignedMemberID: null,
+                assignedMemberName: null
+            };
+            // update the schedule of this member ---> adding this slot to it
+            const member = await MemberModel.findOne({memberID: oldAssignedMemberID});
+            member.schedule = member.schedule.filter((memSlot)=>{
+                return !( memSlot.day==slot.day&&memSlot.time==slot.time&&memSlot.location==slot.location&&memSlot.courseName==course.courseName );
+                
+            });
+            await member.save();
+        }
+    });
+    department.courses.forEach((courseItem,idx)=>{
+        if(courseItem.courseName == course.courseName)
+        {
+            department.courses[idx]=course;
+        }
+    });
+    fac.departments.forEach((dep,idx)=>{
+        if(dep.departmentName == department.departmentName)
+        {
+            fac.departments[idx] = department
+        }
+    });
+    await fac.save();
+});
+
+
+//update
+// input slotID & newMember
+router.put('/assignAcademicMember', async (req,res)=>{
+    // getting the member requesting this get from the data base by the token
+    // and putting it in a variable  req.member using middleware
+    if(req.member.role != "instructor")
+    {
+        return res.status(401).send("Access denied!");
+    }
+    const facultyName = req.member.facultyName;
+    const departmentName = req.member.departmentName;
+    const fac =await FacultyModel.findOne({facultyName: facultyName});
+   // fac.departments;
+    const department = fac.departments.find(dep => dep.departmentName == departmentName);
+    const course = department.courses.find(course => course.courseName == req.body.courseName);
+    if(!isInstructorOfCourse(course,req.member.memberID))
+    {
+        return res.status(401).send("You are not an instructor for this course!");
+    }
+    course.courseSchedule.forEach((slot,idx)=>{
+        if(slot.slotID == req.body.slotID)
+        {
+            if(!slot.assignedMemberID)
+            {
+                return res.status(401).send("This slot is already unassigned!");
+            }
+            const oldAssignedMemberID = slot.assignedMemberID;
+            course.courseSchedule[idx]={
+                slotID: slot.slotID,
+                day: slot.day,
+                time: slot.time,
+                location: slot.location,
+                assignedMemberID: req.body.assignedMemberID,
+                assignedMemberName: req.body.assignedMemberID
+            };
+            // update the schedule of old member ---> removing this slot to it
+            const oldMember = await MemberModel.findOne({memberID: oldAssignedMemberID});
+            oldMember.schedule = oldMember.schedule.filter((memSlot)=>{
+                return !( memSlot.day==slot.day&&memSlot.time==slot.time&&memSlot.location==slot.location&&memSlot.courseName==course.courseName );
+                
+            });
+            await oldMember.save();
+            // update the schedule of new member ---> adding this slot to it
+            const newMember = await MemberModel.findOne({memberID: req.body.assignedMemberID});
+            const tailoredSlot = {
+                day:slot.day,
+                time:slot.time,
+                location:slot.location,
+                courseName:course.courseName
+            };
+            newMember.schedule.push(tailoredSlot);
+            await newMember.save();
+        }
+    });
+    department.courses.forEach((courseItem,idx)=>{
+        if(courseItem.courseName == course.courseName)
+        {
+            department.courses[idx]=course;
+        }
+    });
+    fac.departments.forEach((dep,idx)=>{
+        if(dep.departmentName == department.departmentName)
+        {
+            fac.departments[idx] = department
+        }
+    });
+    await fac.save();
+});
+
+router.delete("/AssignCourseTA", async(req,res,next)=>{
+
+    // dont forget to check the memberRank should be instructor and check the department of the TA to be the same as department of Instructor in the Line Below 
+    
+    memberSchema.findOne({memberId:req.body.memberId},(err,doc)=>{if(err){res.send(err)}; console.log(doc)}).then(async(memberRecord)=>{
+        
+         if (memberRecord)
+            { // U should check that the HOd department and the department the course is in 
+                 CourseSchema.findOne({courseName:req.body.courseName}).then (async(CourseRecord)=>{
+                console.log(CourseRecord)
+                if(CourseRecord)
+                    {
+                       const foundRecord=CourseRecord.instructors.filter((value,index)=>{return value.memberId==memberRecord.memberId})
+                       console.log(foundRecord)
+                        if(foundRecord.length==1)
+                        {    
+                            var NewInstructorData=CourseRecord.instructors.filter((value,index)=>{return value.memberId!=memberRecord.memberId})
+                                CourseSchema.findOneAndUpdate(
+                                    {courseName:req.body.courseName},
+                                    {instructors:NewInstructorData},
+                                     {new: true}
+                                ).then((doc) => {
+                                    console.log(doc);
+                                    res.send(doc)
+                                  }).catch((err)=>{res.send(err)})
+    
+                        }
+                        else
+                        {res.send("The Instructor u want to delete is not assigned this course to be deleted from him/her")}
+                    }
+                    else{res.send("Course data not present in Database")}
+    
+        
+                 }).catch((err)=>{res.send(err)})
+    
+            }
+            else{res.send("Member data not present in Database")
+        
+    
+        }
+    
+    
+            })
+        })
+
 
 router.post('/deleteLocation', async (req,res)=>{
   
