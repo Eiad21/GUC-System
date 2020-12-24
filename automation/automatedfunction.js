e01b7c44c342a301dfe472262e3c078361199d7b
Eiad21 <eiadessam1@gmail.com>
2020-12-24 23:52:09 +0200

    automation

---
+++ automation/automatedfunction.js
@@ -0,0 +1,12 @@
+// setInterval(function() {
+//     console.log("hello"); 
+//    },2000 );
+
+//    setInterval(async function() {
+//     await console.log("bye1");
+//     await console.log("bye2"); 
+//    },1000 );
+
+//     module.exports={}
+
+
