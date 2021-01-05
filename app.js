3d997e87b36af7783b0336226cf14d1abb4cbad9
Omarsawan <o.sawan7@gmail.com>
2021-01-05 16:19:29 +0200

    small fix

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ app.use(express.urlencoded({extended:false}));
 
 
 app.use (function (req,res, next){
-res.header("Access-Control-Allow-Headers" ,"Origin, X-Requested-With, Content-Type, Accept")
   res.header('Access-Control-Allow-Origin',"*");
-  res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With , content-type, Accept")
-  nect();
+  //res.header('Access-Control-Allow-Credentials',"true");
+  res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With , content-type, Accept");
+   //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
 });
 app.use('',Authroutes);
 
